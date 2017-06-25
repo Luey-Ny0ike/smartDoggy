5347176af2fcdf4c0ec372dba055596980bdbecf
lewis nyoike <nyoikelewis@gmail.com>
2017-06-25 14:09:01 +0300

    Modified behaviour of the add new dog form

--- app/components/new-dog/component.js
+++ app/components/new-dog/component.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewDog: false,
+  actions: {
+    dogFormShow(){
+      this.set('addNewDog', true)
+    },
+    dogFormHide(){
+      this.set('addNewDog', false)
+    },
+    saveDog1(){
+      var params={
+        name: this.get('name'),
+        age: this.get('age'),
+        breed: this.get('breed'),
+        color: this.get('color'),
+        title: this.get('title'),
+        nickname: this.get('nickname'),
+        description: this.get('description')
+      };
+      this,set('addNewDog', false);
+      this.sendAction('saveDog2', params)
+    }
+  }
 });
