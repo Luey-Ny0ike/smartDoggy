01b9b5c6ac9f882c414f4b8f50d7ec467d75b7e1
lewis nyoike <nyoikelewis@gmail.com>
2017-06-25 14:25:53 +0300

    Passed the action route handler to admin route

--- app/admin/route.js
+++ app/admin/route.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
       return this.store.findAll('dog');
   },
+  actions:{
+    saveDog3(params){
+      var newdog = this.store.createRecord('dog', params);
+      newdog.save();
+    }
+  }
 });
