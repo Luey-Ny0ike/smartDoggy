d1f87475c5c6faf7f1ad6a4692de467165815c98
lewis nyoike <nyoikelewis@gmail.com>
2017-06-25 22:45:37 +0300

    Passed the function to the admon1 route and transitioned it to the amin route

--- app/admin1/route.js
+++ app/admin1/route.js
@@ -6,6 +6,15 @@ export default Ember.Route.extend({
       var newdog = this.store.createRecord('dog', params);
       newdog.save();
       this.transitionTo('admin');
-    }
+    },
+    update(dog, params) {
+    Object.keys(params).forEach(function(key) {
+      if(params[key]!==undefined) {
+        dog.set(key,params[key]);
+      }
+    });
+    dog.save();
+    this.transitionTo('admin');
+  },
   }
 });
