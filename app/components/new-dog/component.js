57a058306ec44cc775a36c26714a075655f528b3
lewis nyoike <nyoikelewis@gmail.com>
2017-06-25 15:31:06 +0300

    Fixed the save bug although experiencing mixin errors

--- app/components/new-dog/component.js
+++ app/components/new-dog/component.js
@@ -10,14 +10,14 @@ export default Ember.Component.extend({
       this.set('addNewDog', false)
     },
     saveDog1(){
-      var params={
+      var params ={
         name: this.get('name'),
         age: this.get('age'),
         breed: this.get('breed'),
         color: this.get('color'),
         title: this.get('title'),
         nickname: this.get('nickname'),
-        description: this.get('description')
+        description: this.get('description'),
       };
       this.set('addNewDog', false);
       this.sendAction('saveDog2', params)
