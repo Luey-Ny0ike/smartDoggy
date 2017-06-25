187892c6cac478183181820690b990a18b4d0d24
lewis nyoike <nyoikelewis@gmail.com>
2017-06-25 14:19:42 +0300

    passed the save action to the second level

--- app/admin1/route.js
+++ app/admin1/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveDog3(params){
+      var newdog = this.store.createRecord('dog', params);
+      newdog.save();
+      this.transitionTo('admin');
+    }
+  }
 });
