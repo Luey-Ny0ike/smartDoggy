a50194c13679c3557ff43ea70390d8a360d48628
lewis nyoike <nyoikelewis@gmail.com>
2017-06-25 23:11:34 +0300

    FIX THE UPDATE BUG

--- app/admin/route.js
+++ app/admin/route.js
@@ -10,7 +10,14 @@ export default Ember.Route.extend({
       newdog.save();
     },
     update(dog, params) {
-      this.sendAction('update', dog, params);
-    },
+    Object.keys(params).forEach(function(key) {
+      if(params[key]!==undefined) {
+        dog.set(key,params[key]);
+      }
+    });
+    dog.save();
+    this.transitionTo('admin');
+  }
+
   }
 });
