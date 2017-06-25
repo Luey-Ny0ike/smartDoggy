d08a4455416815709f67cf274634ec0432a8a262
lewis nyoike <nyoikelewis@gmail.com>
2017-06-25 22:26:53 +0300

    Passed the actions on how the update form will behave

--- app/components/update-dog/component.js
+++ app/components/update-dog/component.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateDogForm: false,
+  actions: {
+    updateDogForm(){
+      this.set('updateDogForm', true);
+    },
+    updateFormHide(){
+      this.set('updateDogForm', false);
+    },
+    update(dog){
+      var params = {
+        name: this.get('name'),
+        age:  this.get('age'),
+        breed:  this.get('breed'),
+        color:  this.get('color'),
+        image1: this.get('image1'),
+        image2: this.get('image2'),
+        image3: this.get('image3'),
+        nickname:  this.get('nickname'),
+        title: this.get('title'),
+        description: this.get('description'),
+      };
+      this.set('updateDogForm', false);
+      this.sendAction('update', dog, params);
+    }
+  }
 });
