b1e66911f8289dd28af4254dbb2048ccb4ff7eef
lewis nyoike <nyoikelewis@gmail.com>
2017-06-25 12:00:05 +0300

    Created the model to use in the firebase and json file

--- app/dog/model.js
+++ app/dog/model.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+name: DS.attr(),
+age: DS.attr(),
+breed: DS.attr(),
+color: DS.attr(),
+nickname: DS.attr(),
+title: DS.attr(),
+description: DS.attr()
 });
