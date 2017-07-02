ca027a1a1bcc2d1d2fdc4d2d64818aedab63451e
lewis nyoike <nyoikelewis@gmail.com>
2017-07-02 15:46:53 +0300

    Passed dynamic routing functionality in the dog route

--- app/admin1/route.js
+++ app/admin1/route.js
@@ -1,20 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    saveDog3(params){
-      var newdog = this.store.createRecord('dog', params);
-      newdog.save();
-      this.transitionTo('admin');
-    },
-    update(dog, params) {
-    Object.keys(params).forEach(function(key) {
-      if(params[key]!==undefined) {
-        dog.set(key,params[key]);
-      }
-    });
-    dog.save();
-    this.transitionTo('admin');
-  },
-  }
+ model(params){
+   return this.store.findRecord('dog', params.dog_id);
+ },
 });
