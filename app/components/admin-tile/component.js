10f14b75eee769bf467e361cb65a549041e8b248
lewis nyoike <nyoikelewis@gmail.com>
2017-07-02 09:02:41 +0300

    Passed the delete action in the js file

--- app/components/admin-tile/component.js
+++ app/components/admin-tile/component.js
@@ -4,6 +4,11 @@ export default Ember.Component.extend({
   actions:{
     update(dog, params) {
      this.sendAction('update', dog, params);
-   }
+   },
+   delete(dog){
+     if(confirm('Msee, are you sure you want to delete this?')){
+       this.sendAction('destroyDog', dog);
+     }
+    }
   }
 });
