674442ba4f2dd1bb2fbdd11288c27d54af1438d8
lewis nyoike <nyoikelewis@gmail.com>
2017-07-02 17:33:14 +0300

    Fixed the dynamic routing issue working good now

--- app/admin/route.js
+++ app/admin/route.js
@@ -8,19 +8,6 @@ export default Ember.Route.extend({
     saveDog3(params){
       var newdog = this.store.createRecord('dog', params);
       newdog.save();
-    },
-    update(dog, params) {
-    Object.keys(params).forEach(function(key) {
-      if(params[key]!==undefined) {
-        dog.set(key,params[key]);
-      }
-    });
-    dog.save();
-    this.transitionTo('admin');
-  },
-    destroyDog(dog) {
-      dog.destroyRecord();
-      this.transitionTo('admin');
-    }
+    }  
    }
 });
--- app/admin1/route.js
+++ app/admin1/route.js
@@ -4,4 +4,19 @@ export default Ember.Route.extend({
  model(params){
    return this.store.findRecord('dog', params.dog_id);
  },
+ actions:{
+   update(dog, params) {
+   Object.keys(params).forEach(function(key) {
+     if(params[key]!==undefined) {
+       dog.set(key,params[key]);
+     }
+   });
+   dog.save();
+   this.transitionTo('admin');
+ },
+   destroyDog(dog) {
+     dog.destroyRecord();
+     this.transitionTo('admin');
+   }
+ }
 });
