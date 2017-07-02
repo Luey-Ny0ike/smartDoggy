ee03dcf88ced1c0b4ecd64effc6e556286e9f701
lewis nyoike <nyoikelewis@gmail.com>
2017-07-02 17:22:00 +0300

    Removed the update and delete functionality to the detail file

--- app/components/admin-tile/component.js
+++ app/components/admin-tile/component.js
@@ -1,14 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions:{
-    update(dog, params) {
-     this.sendAction('update', dog, params);
-   },
-   delete(dog){
-     if(confirm('Msee, are you sure you want to delete this?')){
-       this.sendAction('destroyDog', dog);
-     }
-    }
-  }
-});
+
+  });
