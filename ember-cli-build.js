c8bae93a7ac1c6dfe50d167962371cd4ddcc41f3
lewis nyoike <nyoikelewis@gmail.com>
2017-07-04 19:23:16 +0300

    Imported the new js file in the ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,6 +10,7 @@ module.exports = function(defaults) {
     }
   });
 
+app.import('public/js/init.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
