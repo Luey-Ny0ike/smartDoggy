9d791dea6bc609bb440bd63c41e8fa210488a0b2
lewis nyoike <nyoikelewis@gmail.com>
2017-07-09 21:19:01 +0300

    Passed the new update params which pushed things to the admin route

--- app/components/admin-puppy-detail/component.js
+++ app/components/admin-puppy-detail/component.js
@@ -6,6 +6,14 @@ export default Ember.Component.extend({
      if(confirm('Msee, are you sure you want to delete this?')){
        this.sendAction('destroyDog', dog);
      }
-   }
+   },
+   update(puppy, params) {
+   Object.keys(params).forEach(function(key) {
+     if(params[key]!==undefined) {
+       puppy.set(key,params[key]);
+     }
+   });
+   puppy.save();
+ },
  }
 });
