c9d86d771bd1eb3560220323e1cfa6d96f85272b
lewis nyoike <nyoikelewis@gmail.com>
2017-07-09 20:26:05 +0300

    fetched all the required params to save a new puppy

--- app/components/new-puppy/component.js
+++ app/components/new-puppy/component.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewPuppy: false,
+  actions: {
+    puppyFormShow(){
+      this.set('addNewPuppy', true)
+    },
+    puppyFormHide(){
+      this.set('addNewPuppy', false)
+    },
+    savePuppy1(){
+      var params ={
+        name: this.get('name'),
+        age: this.get('age'),
+        breed: this.get('breed'),
+        color: this.get('color'),
+        image1: this.get('image1'),
+        image2: this.get('image2'),
+        image3:this.get('image3'),
+        title: this.get('title'),
+        nickname: this.get('nickname'),
+        description: this.get('description'),
+        gender: this.get('gender'),
+        recomendation: this.get('recomendation')
+      };
+      this.set('addNewPuppy', false);
+      this.sendAction('savePuppy2', params)
+    }
+  }
 });
