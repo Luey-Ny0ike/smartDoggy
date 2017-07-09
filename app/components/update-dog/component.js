007c196af0b0a8237f664c178f4a3aeed467e02a
lewis nyoike <nyoikelewis@gmail.com>
2017-07-09 19:56:59 +0300

    Called the recomendation as well as the gender

--- app/components/update-dog/component.js
+++ app/components/update-dog/component.js
@@ -15,11 +15,13 @@ export default Ember.Component.extend({
         age:  this.get('age'),
         breed:  this.get('breed'),
         color:  this.get('color'),
+        gender: this.get('gender'),
         image1: this.get('image1'),
         image2: this.get('image2'),
         image3: this.get('image3'),
         nickname:  this.get('nickname'),
         title: this.get('title'),
+        recomendation: this.get('recomendation'),
         description: this.get('description'),
       };
       this.set('updateDogForm', false);
