af67db8270832fcd289bcfc60e23a256a00fee8d
lewis nyoike <nyoikelewis@gmail.com>
2017-07-09 20:51:49 +0300

    updated the dog model

--- app/dog/model.js
+++ app/dog/model.js
@@ -1,16 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-name: DS.attr(),
-age: DS.attr(),
-breed: DS.attr(),
-color: DS.attr(),
-image1: DS.attr(),
-image2: DS.attr(),
-image3: DS.attr(),
-nickname: DS.attr(),
-title: DS.attr(),
-description: DS.attr(),
-gender: DS.attr(),
-recomendation: DS.attr()
+  name: DS.attr(),
+  age: DS.attr(),
+  breed: DS.attr(),
+  image1: DS.attr(),
+  image2: DS.attr(),
+  image3: DS.attr(),
+  nickname: DS.attr(),
+  title: DS.attr(),
+  description: DS.attr(),
+  gender: DS.attr(),
+  color: DS.attr(),
+  recomendation: DS.attr(),
 });
