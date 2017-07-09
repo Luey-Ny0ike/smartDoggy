7adf495a8f08c87d623891d240d242a6cc42c404
lewis nyoike <nyoikelewis@gmail.com>
2017-07-09 21:06:48 +0300

    Fetched data from update form and sent action to the admin route

--- app/components/update-puppy/component.js
+++ app/components/update-puppy/component.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updatePuppyForm: false,
+  actions: {
+    updatePuppyForm(){
+      this.set('updatePuppyForm', true);
+    },
+    updateFormHide(){
+      this.set('updatePuppyForm', false);
+    },
+    update(puppy){
+      var params = {
+        name: this.get('name'),
+        age:  this.get('age'),
+        breed:  this.get('breed'),
+        color:  this.get('color'),
+        gender: this.get('gender'),
+        image1: this.get('image1'),
+        image2: this.get('image2'),
+        image3: this.get('image3'),
+        nickname:  this.get('nickname'),
+        title: this.get('title'),
+        recomendation: this.get('recomendation'),
+        description: this.get('description'),
+      };
+      this.set('updatePuppyForm', false);
+      this.sendAction('update', puppy, params);
+    }
+  }
 });
