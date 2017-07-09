90af3fae9569c8c107b4ef120263d0da92fdec68
lewis nyoike <nyoikelewis@gmail.com>
2017-07-09 15:46:11 +0300

    Regenerated the deleted puppy model

---
+++ app/puppy/model.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr(),
+  age: DS.attr(),
+  breed: DS.attr(),
+  image1: DS.attr(),
+  image2: DS.attr(),
+  image3: DS.attr(),
+  nickname: DS.attr(),
+  title: DS.attr(),
+  description: DS.attr(),
+  gender: DS.attr(),
+  color: DS.attr(),
+});
