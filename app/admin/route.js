04cd5ccbf09016e70ab4d0f9c45121019d5c4859
lewis nyoike <nyoikelewis@gmail.com>
2017-07-10 18:53:09 +0300

    passed functionality for login and logout

--- app/admin/route.js
+++ app/admin/route.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+firebase:Ember.inject.service(),
   model() {
     return Ember.RSVP.hash({
       dogs: this.store.findAll('dog'),
@@ -17,6 +18,21 @@ export default Ember.Route.extend({
       var newpuppy = this.store.createRecord('puppy', params);
       newpuppy.save();
       this.transitionTo('admin');
+   },
+
+   login(){
+     this.get('session').open('firebase', {
+       provider: "password",
+       email:"nyoikelewis@gmail.com",
+       password:"1234567"
+     }).then(function(){
+       console.log("success logging in")
+     }).catch(function(){
+       console.log("failed to login")
+     });
+   },
+   logout(){
+     this.get('session').close();
    }
  }
 });
