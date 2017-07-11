403c41d0c1efa935ac3c06d54bbe41aab0f82722
lewis nyoike <nyoikelewis@gmail.com>
2017-07-11 21:50:24 +0300

    Passed actions to fetch user input from the form

--- app/components/login-form/component.js
+++ app/components/login-form/component.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  	firebase: Ember.inject.service(),
+  actions:{
+		logIn(){
+			var user={
+				ema:this.get('email'),
+				password:this.get('password')
+			}
+			this.sendAction('logIn', user);
+		}
+  }
 });
