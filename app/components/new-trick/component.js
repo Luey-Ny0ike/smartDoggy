ab366d88ee5788c4446639fb57c6b6d0aa84fa5f
lewis nyoike <nyoikelewis@gmail.com>
2017-07-11 23:14:15 +0300

    Added functionality to hide and show form as well as fetch input from forms and pass them to the admin route

--- app/components/new-trick/component.js
+++ app/components/new-trick/component.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewTrick: false,
+  actions: {
+    trickFormShow() {
+      this.set('addNewTrick', true)
+    },
+    trickFormHide() {
+      this.set('addNewTrick', false)
+    },
+    saveTrick1() {
+      var params = {
+        title: this.get('title'),
+        content1: this.get('content1'),
+        content2: this.get('content2'),
+        image1: this.get('image1'),
+        image2: this.get('image2'),
+        video: this.get('video'),
+      };
+      this.set('addNewTrick', false);
+      this.sendAction('saveTrick2', params)
+    }
+  }
 });
