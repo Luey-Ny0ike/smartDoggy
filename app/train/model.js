cb41955fbebf5743fdf144dc4bc6083625195f42
lewis nyoike <nyoikelewis@gmail.com>
2017-07-11 22:04:52 +0300

    added attributes for the train model

--- app/train/model.js
+++ app/train/model.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+title: DS.attr(),
+content1: DS.attr(),
+content2: DS.attr(),
+image1: DS.attr(),
+image2: DS.attr(),
+video: DS.attr()
 });
