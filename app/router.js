82d78f226353f6258ecff334096d8d635dbe84d8
lewis nyoike <nyoikelewis@gmail.com>
2017-07-11 21:58:35 +0300

    Deleted the nested routes created earlier are their are no longer needed

--- app/router.js
+++ app/router.js
@@ -8,10 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('contact');
-  this.route('train', function() {
-    this.route('sit');
-    this.route('bark');
-  });
+  this.route('train');
   this.route('admin');
   this.route('admin1', {path: '/admin1/:dog_id'});
   this.route('dog', {path:'/dog/:dog_id'});
