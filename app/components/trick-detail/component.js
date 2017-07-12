dba938d5dc494327dff21504057947d08270f16a
lewis nyoike <nyoikelewis@gmail.com>
2017-07-12 08:55:58 +0300

    Passed a function to display a confirm deletion method before sending the action to the route handler

--- app/components/trick-detail/component.js
+++ app/components/trick-detail/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions:{
+   delete(trick){
+     if(confirm('Msee, are you sure you want to delete this?')){
+       this.sendAction('destroyTrick', trick);
+     }
+   }
+ }
 });
