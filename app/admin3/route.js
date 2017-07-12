1bf57844162c80bf3c56d21a044160b6e19c90e3
lewis nyoike <nyoikelewis@gmail.com>
2017-07-12 08:57:22 +0300

    Passed action to delete the record of the trick

--- app/admin3/route.js
+++ app/admin3/route.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('trick', params.trick_id);
+  },
+  actions:{
+  destroyTrick(trick) {
+    trick.destroyRecord();
+    this.transitionTo('admin');
   }
+}
 });
