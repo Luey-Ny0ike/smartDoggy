c2a89256908f7b6001526c2009295f38f257e1ff
lewis nyoike <nyoikelewis@gmail.com>
2017-07-13 09:44:11 +0300

    Fixed the delete functionality

--- app/components/admin-puppy-detail/component.js
+++ app/components/admin-puppy-detail/component.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions:{
-   delete(dog){
+   delete(puppy){
      if(confirm('Msee, are you sure you want to delete this?')){
-       this.sendAction('destroyDog', dog);
+       this.sendAction('destroyPuppy', puppy);
      }
    },
    update(puppy, params) {
