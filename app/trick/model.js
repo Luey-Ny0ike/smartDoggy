f33159a3399b80555bf41aab644a5ef6cff512bc
lewis nyoike <nyoikelewis@gmail.com>
2017-07-13 09:34:55 +0300

    Updated the model to accomodate the new inputs

--- app/trick/model.js
+++ app/trick/model.js
@@ -2,9 +2,12 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 title: DS.attr(),
-content1: DS.attr(),
-content2: DS.attr(),
+step1: DS.attr(),
+step2: DS.attr(),
+step3:DS.attr(),
+step4:DS.attr(),
 image1: DS.attr(),
 image2: DS.attr(),
+extratips:DS.attr(),
 video: DS.attr()
 });
