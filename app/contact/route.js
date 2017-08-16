2b18c0c1b8e21eb552b37428fbd8e6c2f987bbc6
lewis nyoike <nyoikelewis@gmail.com>
2017-08-16 22:12:36 +0300

    Fetched the params from the from the contact form and used its parameters to create a new comment mixin errors are still being experienced when creating new comment

--- app/contact/route.js
+++ app/contact/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveComment() {
+      var params = {
+        firstname: this.get('firstname'),
+        lastname: this.get('lastname'),
+        email: this.get('email'),
+        comment: this.get('comment')
+      }
+    },
+
+    saveComment(params) {
+      var newcomment = this.store.createRecord('comment', params);
+      newcomment.save();
+      this.transitionTo('contact')
+    }
+  }
 });
